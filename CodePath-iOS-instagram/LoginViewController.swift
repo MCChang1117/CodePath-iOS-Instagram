f049f14ff9212a950e9f2bbc54d0ebb986a4e5b8
MCChang macbook <meng.chuan.chang@gmail.com>
2021-10-22 19:33:54 -0400

    add func of closing keyboard in Login page
    The keyboard would block sign up and sign in button and user cannot close it

--- CodePath-iOS-instagram/LoginViewController.swift
+++ CodePath-iOS-instagram/LoginViewController.swift
@@ -16,8 +16,19 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Looks for single or multiple taps.
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
 
+        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
+        //tap.cancelsTouchesInView = false
+
+        view.addGestureRecognizer(tap)
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
     
     @IBAction func onSignIn(_ sender: Any) {
