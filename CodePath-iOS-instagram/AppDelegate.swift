96c90b98abdd00ee4412ce97360c653079afcda2
MCChang macbook <meng.chuan.chang@gmail.com>
2021-10-22 15:35:10 -0400

    Signing up and Logging In

--- CodePath-iOS-instagram/AppDelegate.swift
+++ CodePath-iOS-instagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "IBxFIfhpC7AeQ3juZI7SWXAQHJdqHdDReUP2sW7t"
+                    $0.clientKey = "eGQoRjOoot7SvWi5dSHiMXQzxwfHUyMuRKuR0SV2"
+                    $0.server = "https://parseapi.back4app.com"
+            }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
---
+++ CodePath-iOS-instagram/FeedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FeedViewController.swift
+//  CodePath-iOS-instagram
+//
+//  Created by Meng-Chuan Chang on 10/22/21.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
